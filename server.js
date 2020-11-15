852ea76b1955b5e92b81b4e16776bf9d4a600dc6
BajaJustin <justindabaja@gmail.com>
2020-11-15 12:28:34 -0500

    Added validation check and error handling

--- server.js
+++ server.js
@@ -14,15 +14,48 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function (req, res, next) {
   res.sendFile(__dirname + '/views/index.html');
+  next();
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/timestamp/:date?', function (req, res) {
+  console.log(req.params);
+  // User entered paramerters 
+  const USERDATE = req.params.date;
+  var date;
+
+  // Handle empty paramters
+  if(!USERDATE){
+    // Set date to today's day if none provided 
+    date = new Date();
+  }else {
+    // Check for valid date
+    if(!isNaN(USERDATE)){
+      // Parameter entered is a number
+      date = new Date(parseInt(USERDATE));
+    }else {
+      // Parameter is a string
+      date = new Date(USERDATE);
+    }
+  }
+
+  // Checks if valid date was entered 
+  if(date.toString() === 'Invalid Date'){
+    // Invalid date was entered, return json error message
+    res.json({ error: 'Invalid Date' });
+  }else {
+    // Displays json formated date, both in unix and utc
+    res.json( { unix: date.getTime(), utc: date.toUTCString() } );
+  }
 });
+
+// Non-existing routes
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/views/error.html')
+})
 
 
 
